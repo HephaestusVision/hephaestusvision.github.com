b22e0e382e4dceab8cd20b35f87cc8f729359f10
Hal Canary <hal@cs.unc.edu>
2012-12-09 19:21:43 -0500

    update docs

--- scanner_maintenance_documentation/search/functions_72.js
+++ scanner_maintenance_documentation/search/functions_72.js
@@ -3,5 +3,7 @@ var searchData=
   ['resetcamera',['ResetCamera',['../classQVTKPolyViewWidget.html#a2afc25f85fa3e8c566bf16c57789c8d0',1,'QVTKPolyViewWidget']]],
   ['resettodefaults',['resetToDefaults',['../classParameters.html#ae7f1dd80cb33d4d11e38ae1aad75d067',1,'Parameters']]],
   ['resizegl',['resizeGL',['../classCameraWidget.html#acb6cb4c0b9196dbf82811c0888b2caa7',1,'CameraWidget']]],
+  ['revert',['revert',['../classDepthScannerMainWindow.html#ad9e5de8e537ebdba6a19a55a11176f52',1,'DepthScannerMainWindow']]],
+  ['revertpointcloud',['RevertPointCloud',['../classCloudy.html#aeee9a2a6600852f8f9b67013fef21b3e',1,'Cloudy']]],
   ['rundialog',['runDialog',['../MidasUpload_8h.html#a3f1f13a643d74ee503c17b9795876c6f',1,'MidasUpload.cxx']]]
 ];
